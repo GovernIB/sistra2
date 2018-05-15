83baa677496e1e6b5ef15909c9def3f094e84fc5
amaciam-indra <amaciam@indra.es>
2018-05-15 19:55:08 +0200

    #19 Actualización del modelo de datos

--- sistrages/scripts/bbdd/1.0/oracle/sistrages_create_data.sql
+++ sistrages/scripts/bbdd/1.0/oracle/sistrages_create_data.sql
@@ -3,4 +3,4 @@ insert into STG_IDIOMA (IDI_IDENTI) VALUES ('ca');
 insert into STG_IDIOMA (IDI_IDENTI) VALUES ('en');
 insert into STG_IDIOMA (IDI_IDENTI) VALUES ('de');
 
-Insert into STG_CNFGLO (CFG_CODIGO,CFG_PROP,CFG_VALOR,CFG_DESCR) values (STG_CNFGLO_SEQ.NEXTVAL,'ficherosExternos.path','\opt\sistra2\files','Almácenamiento ficheros');
+Insert into STG_PROCES (PROC_IDENT,PROC_INSTAN,PROC_FECHA) values ('MAESTRO','NONE', sysdate);
