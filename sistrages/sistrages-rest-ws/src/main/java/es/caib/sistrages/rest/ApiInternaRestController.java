c2bb8623376288a6b02f577dbba489569b15ea9a
jmico-indra <jmico@indra.es>
2018-12-10 17:25:50 +0100

    #64 Cambios realizados:
     - Revisión dominios (indicador caché para test, ajuste parámetros
    fuente datos, activación cron refresco)

--- sistrages/sistrages-rest-ws/src/main/java/es/caib/sistrages/rest/ApiInternaRestController.java
+++ sistrages/sistrages-rest-ws/src/main/java/es/caib/sistrages/rest/ApiInternaRestController.java
@@ -49,180 +49,182 @@ import io.swagger.annotations.ApiOperation;
 @Api(value = "interna", produces = "application/json")
 public class ApiInternaRestController {
 
-	/** Servicio negocio. */
-	@Autowired
-	private RestApiInternaService restApiService;
+    /** Servicio negocio. */
+    @Autowired
+    private RestApiInternaService restApiService;
 
-	/**
-	 * ConfiguracionGlobalAdapter
-	 */
-	@Autowired
-	private ConfiguracionGlobalAdapter confGlobalAdapter;
+    /**
+     * ConfiguracionGlobalAdapter
+     */
+    @Autowired
+    private ConfiguracionGlobalAdapter confGlobalAdapter;
 
-	/**
-	 * ConfiguracionEntidadAdapter
-	 */
-	@Autowired
-	private ConfiguracionEntidadAdapter confEntidadAdapter;
+    /**
+     * ConfiguracionEntidadAdapter
+     */
+    @Autowired
+    private ConfiguracionEntidadAdapter confEntidadAdapter;
 
-	/**
-	 * VersionTramiteAdapter
-	 */
-	@Autowired
-	private VersionTramiteAdapter versionTramiteAdapter;
+    /**
+     * VersionTramiteAdapter
+     */
+    @Autowired
+    private VersionTramiteAdapter versionTramiteAdapter;
 
-	/**
-	 * AvisosEntidadAdapter
-	 */
-	@Autowired
-	private AvisosEntidadAdapter avisosEntidadAdapter;
+    /**
+     * AvisosEntidadAdapter
+     */
+    @Autowired
+    private AvisosEntidadAdapter avisosEntidadAdapter;
 
-	/**
-	 * ValoresDominioAdapter
-	 */
-	@Autowired
-	private ValoresDominioAdapter valoresDominioAdapter;
+    /**
+     * ValoresDominioAdapter
+     */
+    @Autowired
+    private ValoresDominioAdapter valoresDominioAdapter;
 
-	@Autowired
-	private RolesAdapter rolesAdapter;
+    @Autowired
+    private RolesAdapter rolesAdapter;
 
-	// TODO Hacer que todas las clases del modelo api rest empiecen con "R" para
-	// evitar conflictos con modelo core? RENOMBRAMOS QUE EMPIECEN CON "R"
+    // TODO Hacer que todas las clases del modelo api rest empiecen con "R" para
+    // evitar conflictos con modelo core? RENOMBRAMOS QUE EMPIECEN CON "R"
 
-	// TODO Ver gestion errores, tanto generados en capa Rest como los que
-	// vengan de negocio. Ver de gestionar con interceptor. DE MOMENTO QUE
-	// DEVUELVA 500 Y LUEGO YA VEMOS.
+    // TODO Ver gestion errores, tanto generados en capa Rest como los que
+    // vengan de negocio. Ver de gestionar con interceptor. DE MOMENTO QUE
+    // DEVUELVA 500 Y LUEGO YA VEMOS.
 
-	/**
-	 * Recupera configuración global.
-	 *
-	 * @return Configuracion global
-	 */
+    /**
+     * Recupera configuración global.
+     *
+     * @return Configuracion global
+     */
 
-	@ApiOperation(value = "Lista de Propiedades de configuracion global", notes = "Lista de Propiedades de configuracion global", response = RConfiguracionGlobal.class)
-	@RequestMapping(value = "/configuracionGlobal", method = RequestMethod.GET)
-	public RConfiguracionGlobal obtenerConfiguracionGlobal() {
-		final List<ConfiguracionGlobal> cg = restApiService.listConfiguracionGlobal(null);
-		final List<Plugin> pg = restApiService.listPlugin(TypeAmbito.GLOBAL, (long) 0, null);
-		return confGlobalAdapter.convertir(cg, pg);
-	}
+    @ApiOperation(value = "Lista de Propiedades de configuracion global", notes = "Lista de Propiedades de configuracion global", response = RConfiguracionGlobal.class)
+    @RequestMapping(value = "/configuracionGlobal", method = RequestMethod.GET)
+    public RConfiguracionGlobal obtenerConfiguracionGlobal() {
+        final List<ConfiguracionGlobal> cg = restApiService
+                .listConfiguracionGlobal(null);
+        final List<Plugin> pg = restApiService.listPlugin(TypeAmbito.GLOBAL,
+                (long) 0, null);
+        return confGlobalAdapter.convertir(cg, pg);
+    }
 
-	/**
-	 * Recupera configuración entidad.
-	 *
-	 * @param codigoDIR3
-	 *            id entidad
-	 * @return Entidad
-	 */
-	@ApiOperation(value = "Lista de Propiedades de configuracion de entidad", notes = "Lista de Propiedades de configuracion de entidad", response = RConfiguracionEntidad.class)
-	@RequestMapping(value = "/entidad/{id}", method = RequestMethod.GET)
-	public RConfiguracionEntidad obtenerConfiguracionEntidad(@PathVariable("id") final String codigoDIR3) {
-		final Entidad entidad = restApiService.loadEntidad(codigoDIR3);
-		final List<FormularioSoporte> formSoporte = restApiService.listOpcionesFormularioSoporte(entidad.getCodigo());
-		return confEntidadAdapter.convertir(entidad, formSoporte);
-	}
+    /**
+     * Recupera configuración entidad.
+     *
+     * @param codigoDIR3
+     *            id entidad
+     * @return Entidad
+     */
+    @ApiOperation(value = "Lista de Propiedades de configuracion de entidad", notes = "Lista de Propiedades de configuracion de entidad", response = RConfiguracionEntidad.class)
+    @RequestMapping(value = "/entidad/{id}", method = RequestMethod.GET)
+    public RConfiguracionEntidad obtenerConfiguracionEntidad(
+            @PathVariable("id") final String codigoDIR3) {
+        final Entidad entidad = restApiService.loadEntidad(codigoDIR3);
+        final List<FormularioSoporte> formSoporte = restApiService
+                .listOpcionesFormularioSoporte(entidad.getCodigo());
+        return confEntidadAdapter.convertir(entidad, formSoporte);
+    }
 
-	/**
-	 * Recupera definición versión de trámite.
-	 *
-	 * @param idioma
-	 *            Idioma
-	 * @param idtramite
-	 *            Id Trámite
-	 * @param version
-	 *            Versión trámite
-	 * @return versión de trámite
-	 * @throws Exception
-	 */
-	@ApiOperation(value = "Obtiene la definición de la versión del tramite", notes = "Obtiene la definición de la versión del tramite", response = RVersionTramite.class)
-	@RequestMapping(value = "/tramite/{idTramite}/{version}/{idioma}", method = RequestMethod.GET)
-	public RVersionTramite obtenerDefinicionVersionTramite(@PathVariable("idTramite") final String idtramite,
-			@PathVariable("version") final int version, @PathVariable("idioma") final String idioma) throws Exception {
+    /**
+     * Recupera definición versión de trámite.
+     *
+     * @param idioma
+     *            Idioma
+     * @param idtramite
+     *            Id Trámite
+     * @param version
+     *            Versión trámite
+     * @return versión de trámite
+     * @throws Exception
+     */
+    @ApiOperation(value = "Obtiene la definición de la versión del tramite", notes = "Obtiene la definición de la versión del tramite", response = RVersionTramite.class)
+    @RequestMapping(value = "/tramite/{idTramite}/{version}/{idioma}", method = RequestMethod.GET)
+    public RVersionTramite obtenerDefinicionVersionTramite(
+            @PathVariable("idTramite") final String idtramite,
+            @PathVariable("version") final int version,
+            @PathVariable("idioma") final String idioma) throws Exception {
 
-		final String idiomaDefecto = restApiService.getValorConfiguracionGlobal("definicionTramite.lenguajeDefecto");
-		final TramiteVersion tv = restApiService.loadTramiteVersion(idtramite, version);
-		if (tv == null) {
-			throw new Exception("El tramite especificado no existe");
-		}
+        final String idiomaDefecto = restApiService.getValorConfiguracionGlobal(
+                "definicionTramite.lenguajeDefecto");
+        final TramiteVersion tv = restApiService.loadTramiteVersion(idtramite,
+                version);
+        if (tv == null) {
+            throw new Exception("El tramite especificado no existe");
+        }
 
-		return versionTramiteAdapter.convertir(idtramite, tv, idioma, idiomaDefecto);
-	}
+        return versionTramiteAdapter.convertir(idtramite, tv, idioma,
+                idiomaDefecto);
+    }
 
-	/**
-	 * Obtiene avisos activos entidad.
-	 *
-	 * @param idEntidad
-	 *            Id entidad
-	 * @return avisos
-	 */
-	@ApiOperation(value = "Obtiene los avisos de una entidad", notes = "Obtiene los avisos de una entidad", response = RAvisosEntidad.class)
-	@RequestMapping(value = "/entidad/{id}/avisos", method = RequestMethod.GET)
-	public RAvisosEntidad obtenerAvisosEntidad(@PathVariable("id") final String idEntidad) {
-		return avisosEntidadAdapter.convertir(idEntidad);
-	}
+    /**
+     * Obtiene avisos activos entidad.
+     *
+     * @param idEntidad
+     *            Id entidad
+     * @return avisos
+     */
+    @ApiOperation(value = "Obtiene los avisos de una entidad", notes = "Obtiene los avisos de una entidad", response = RAvisosEntidad.class)
+    @RequestMapping(value = "/entidad/{id}/avisos", method = RequestMethod.GET)
+    public RAvisosEntidad obtenerAvisosEntidad(
+            @PathVariable("id") final String idEntidad) {
+        return avisosEntidadAdapter.convertir(idEntidad);
+    }
 
-	/**
-	 * Recupera valores de un dominio de fuente de datos.
-	 *
-	 * @param idDominio
-	 *            id dominio
-	 * @param parametrosJSON
-	 *            parametros (en formato JSON)
-	 * @return Valores dominio
-	 */
-	@ApiOperation(value = "Obtiene los valores de un dominio", notes = "Obtiene los valores FD de un dominio", response = RValoresDominio.class)
-	@RequestMapping(value = "/dominioFuenteDatos/{idDominio}", method = RequestMethod.GET)
-	public RValoresDominio obtenerValoresDominioFD(@PathVariable("idDominio") final String idDominio,
-			@RequestBody(required = false) final RListaParametros parametros) {
+    /**
+     * Recupera valores de un dominio de fuente de datos.
+     *
+     * @param idDominio
+     *            id dominio
+     * @param parametrosJSON
+     *            parametros (en formato JSON)
+     * @return Valores dominio
+     */
+    @ApiOperation(value = "Obtiene los valores de un dominio", notes = "Obtiene los valores FD de un dominio", response = RValoresDominio.class)
+    @RequestMapping(value = "/dominioFuenteDatos/{idDominio}", method = RequestMethod.POST)
+    public RValoresDominio obtenerValoresDominioFD(
+            @PathVariable("idDominio") final String idDominio,
+            @RequestBody(required = false) final RListaParametros parametros) {
 
-		// Convertimos los parametros a la clase necesaria
-		final List<ValorParametroDominio> listaParams = new ArrayList<>();
-		if (parametros != null && parametros.getParametros() != null) {
-			for (final RValorParametro p : parametros.getParametros()) {
-				final ValorParametroDominio param = new ValorParametroDominio();
-				param.setCodigo(p.getCodigo());
-				param.setValor(p.getValor());
-				listaParams.add(param);
-			}
-		}
+        // Convertimos los parametros a la clase necesaria
+        final List<ValorParametroDominio> listaParams = new ArrayList<>();
+        if (parametros != null && parametros.getParametros() != null) {
+            for (final RValorParametro p : parametros.getParametros()) {
+                final ValorParametroDominio param = new ValorParametroDominio();
+                param.setCodigo(p.getCodigo());
+                param.setValor(p.getValor());
+                listaParams.add(param);
+            }
+        }
 
-		final ValoresDominio res = restApiService.realizarConsultaFuenteDatos(idDominio, listaParams);
-		return valoresDominioAdapter.convertir(res);
-	}
+        final ValoresDominio res = restApiService
+                .realizarConsultaFuenteDatos(idDominio, listaParams);
+        return valoresDominioAdapter.convertir(res);
+    }
 
-	/**
-	 * Recupera valores de un dominio de fuente de datos.
-	 *
-	 * @param idDominio
-	 *            id dominio
-	 * @param parametrosJSON
-	 *            parametros (en formato JSON)
-	 * @return Valores dominio
-	 */
-	@ApiOperation(value = "Obtiene los valores de un dominio", notes = "Obtiene los valores LF de un dominio", response = RValoresDominio.class)
-	@RequestMapping(value = "/dominioListaFija/{idDominio}", method = RequestMethod.GET)
-	public RValoresDominio obtenerValoresDominioLF(@PathVariable("idDominio") final String idDominio,
-			@RequestBody(required = false) final RListaParametros parametros) {
+    /**
+     * Recupera valores de un dominio de fuente de datos.
+     *
+     * @param idDominio
+     *            id dominio
+     * @param parametrosJSON
+     *            parametros (en formato JSON)
+     * @return Valores dominio
+     */
+    @ApiOperation(value = "Obtiene los valores de un dominio", notes = "Obtiene los valores LF de un dominio", response = RValoresDominio.class)
+    @RequestMapping(value = "/dominioListaFija/{idDominio}", method = RequestMethod.GET)
+    public RValoresDominio obtenerValoresDominioLF(
+            @PathVariable("idDominio") final String idDominio) {
 
-		// Convertimos los parametros a la clase necesaria
-		final List<ValorParametroDominio> listaParams = new ArrayList<>();
-		if (parametros != null && parametros.getParametros() != null) {
-			for (final RValorParametro p : parametros.getParametros()) {
-				final ValorParametroDominio param = new ValorParametroDominio();
-				param.setCodigo(p.getCodigo());
-				param.setValor(p.getValor());
-				listaParams.add(param);
-			}
-		}
+        final ValoresDominio res = restApiService
+                .realizarConsultaListaFija(idDominio);
+        return valoresDominioAdapter.convertir(res);
+    }
 
-		final ValoresDominio res = restApiService.realizarConsultaListaFija(idDominio, listaParams);
-		return valoresDominioAdapter.convertir(res);
-	}
-
-	@ApiOperation(value = "Lista de roles con permiso helpdesk", notes = "Lista de roles con permiso helpdesk", response = RPermisoHelpDesk.class, responseContainer = "List")
-	@RequestMapping(value = "/permisosHelpdesk", method = RequestMethod.GET)
-	public List<RPermisoHelpDesk> obtenerPermisosHelpdesk() {
-		final List<Rol> roles = restApiService.obtenerPermisosHelpdesk();
-		return rolesAdapter.convertir(roles);
-	}
+    @ApiOperation(value = "Lista de roles con permiso helpdesk", notes = "Lista de roles con permiso helpdesk", response = RPermisoHelpDesk.class, responseContainer = "List")
+    @RequestMapping(value = "/permisosHelpdesk", method = RequestMethod.GET)
+    public List<RPermisoHelpDesk> obtenerPermisosHelpdesk() {
+        final List<Rol> roles = restApiService.obtenerPermisosHelpdesk();
+        return rolesAdapter.convertir(roles);
+    }
 }
--- sistramit/sistramit-core/src/main/java/es/caib/sistramit/core/service/component/integracion/SistragesApiComponentImpl.java
+++ sistramit/sistramit-core/src/main/java/es/caib/sistramit/core/service/component/integracion/SistragesApiComponentImpl.java
@@ -1,8 +1,15 @@
 package es.caib.sistramit.core.service.component.integracion;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.support.BasicAuthorizationInterceptor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -11,10 +18,13 @@ import es.caib.sistrages.rest.api.interna.RAvisosEntidad;
 import es.caib.sistrages.rest.api.interna.RConfiguracionEntidad;
 import es.caib.sistrages.rest.api.interna.RConfiguracionGlobal;
 import es.caib.sistrages.rest.api.interna.RDominio;
+import es.caib.sistrages.rest.api.interna.RListaParametros;
+import es.caib.sistrages.rest.api.interna.RValorParametro;
 import es.caib.sistrages.rest.api.interna.RValoresDominio;
 import es.caib.sistrages.rest.api.interna.RVersionTramite;
 import es.caib.sistramit.core.api.model.system.types.TypePropiedadConfiguracion;
 import es.caib.sistramit.core.service.component.system.ConfiguracionComponent;
+import es.caib.sistramit.core.service.model.integracion.ParametrosDominio;
 
 /**
  * Implementación acceso SISTRAGES.
@@ -25,88 +35,134 @@ import es.caib.sistramit.core.service.component.system.ConfiguracionComponent;
 @Component("sistragesApiComponent")
 public final class SistragesApiComponentImpl implements SistragesApiComponent {
 
-	/** Log. */
-	private final Logger log = LoggerFactory.getLogger(getClass());
+    /** Log. */
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
-	/** Configuracion. */
-	@Autowired
-	private ConfiguracionComponent configuracionComponent;
+    /** Configuracion. */
+    @Autowired
+    private ConfiguracionComponent configuracionComponent;
 
-	@Override
-	public RConfiguracionGlobal obtenerConfiguracionGlobal() {
-		final RestTemplate restTemplate = new RestTemplate();
-		restTemplate.getInterceptors().add(new BasicAuthorizationInterceptor(getUser(), getPassword()));
-		return restTemplate.getForObject(getUrl() + "/configuracionGlobal", RConfiguracionGlobal.class);
-	}
+    @Override
+    public RConfiguracionGlobal obtenerConfiguracionGlobal() {
+        final RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getInterceptors().add(
+                new BasicAuthorizationInterceptor(getUser(), getPassword()));
+        return restTemplate.getForObject(getUrl() + "/configuracionGlobal",
+                RConfiguracionGlobal.class);
+    }
 
-	@Override
-	public RConfiguracionEntidad obtenerConfiguracionEntidad(final String idEntidad) {
+    @Override
+    public RConfiguracionEntidad obtenerConfiguracionEntidad(
+            final String idEntidad) {
 
-		final RestTemplate restTemplate = new RestTemplate();
-		restTemplate.getInterceptors().add(new BasicAuthorizationInterceptor(getUser(), getPassword()));
-		return restTemplate.getForObject(getUrl() + "/entidad/" + idEntidad, RConfiguracionEntidad.class);
-	}
+        final RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getInterceptors().add(
+                new BasicAuthorizationInterceptor(getUser(), getPassword()));
+        return restTemplate.getForObject(getUrl() + "/entidad/" + idEntidad,
+                RConfiguracionEntidad.class);
+    }
 
-	@Override
-	public RVersionTramite recuperarDefinicionTramite(final String idTramite, final int version, final String idioma) {
-		final RestTemplate restTemplate = new RestTemplate();
-		restTemplate.getInterceptors().add(new BasicAuthorizationInterceptor(getUser(), getPassword()));
-		return restTemplate.getForObject(getUrl() + "/tramite/" + idTramite + "/" + version + "/" + idioma,
-				RVersionTramite.class);
+    @Override
+    public RVersionTramite recuperarDefinicionTramite(final String idTramite,
+            final int version, final String idioma) {
+        final RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getInterceptors().add(
+                new BasicAuthorizationInterceptor(getUser(), getPassword()));
+        return restTemplate.getForObject(getUrl() + "/tramite/" + idTramite
+                + "/" + version + "/" + idioma, RVersionTramite.class);
 
-	}
+    }
 
-	@Override
-	public RAvisosEntidad obtenerAvisosEntidad(final String idEntidad) {
-		final RestTemplate restTemplate = new RestTemplate();
-		restTemplate.getInterceptors().add(new BasicAuthorizationInterceptor(getUser(), getPassword()));
-		return restTemplate.getForObject(getUrl() + "/entidad/" + idEntidad + "/avisos", RAvisosEntidad.class);
-	}
+    @Override
+    public RAvisosEntidad obtenerAvisosEntidad(final String idEntidad) {
+        final RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getInterceptors().add(
+                new BasicAuthorizationInterceptor(getUser(), getPassword()));
+        return restTemplate.getForObject(
+                getUrl() + "/entidad/" + idEntidad + "/avisos",
+                RAvisosEntidad.class);
+    }
 
-	@Override
-	public RValoresDominio resuelveDominioFuenteDatos(final RDominio dominio) {
-		final RestTemplate restTemplate = new RestTemplate();
-		restTemplate.getInterceptors().add(new BasicAuthorizationInterceptor(getUser(), getPassword()));
-		return restTemplate.getForObject(getUrl() + "/dominioFuenteDatos/" + dominio.getIdentificador(),
-				RValoresDominio.class);
+    @Override
+    public RValoresDominio resuelveDominioFuenteDatos(final RDominio dominio,
+            final ParametrosDominio parametrosDominio) {
 
-	}
+        // Convertimos lista parametros
+        final List<RValorParametro> parametros = new ArrayList<>();
+        if (parametrosDominio != null
+                && parametrosDominio.getParametros() != null
+                && !parametrosDominio.getParametros().isEmpty()) {
+            for (final es.caib.sistramit.core.service.model.integracion.ParametroDominio parametroDominio : parametrosDominio
+                    .getParametros()) {
+                final RValorParametro rValorParametro = new RValorParametro();
+                rValorParametro.setCodigo(parametroDominio.getCodigo());
+                rValorParametro.setValor(parametroDominio.getValor());
+                parametros.add(rValorParametro);
+            }
+        }
+        final RListaParametros listaParametros = new RListaParametros();
+        listaParametros.setParametros(parametros);
 
-	@Override
-	public RValoresDominio resuelveDominioListaFija(final RDominio dominio) {
-		final RestTemplate restTemplate = new RestTemplate();
-		restTemplate.getInterceptors().add(new BasicAuthorizationInterceptor(getUser(), getPassword()));
-		return restTemplate.getForObject(getUrl() + "/dominioListaFija/" + dominio.getIdentificador(),
-				RValoresDominio.class);
+        // Realizamos llamada
+        final RestTemplate restTemplate = new RestTemplate();
+        final String url = getUrl();
+        final String usuario = getUser();
+        final String pass = getPassword();
+        restTemplate.getInterceptors()
+                .add(new BasicAuthorizationInterceptor(usuario, pass));
+        final HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        final HttpEntity<RListaParametros> request = new HttpEntity<>(
+                listaParametros, headers);
+        final ResponseEntity<RValoresDominio> responseRest = restTemplate
+                .postForEntity(
+                        url + "/dominioFuenteDatos/"
+                                + dominio.getIdentificador(),
+                        request, RValoresDominio.class);
+        return responseRest.getBody();
+    }
 
-	}
+    @Override
+    public RValoresDominio resuelveDominioListaFija(final RDominio dominio) {
 
-	/**** Private functions. **/
-	/**
-	 * Obtiene el password.
-	 *
-	 * @return
-	 */
-	private String getPassword() {
-		return configuracionComponent.obtenerPropiedadConfiguracion(TypePropiedadConfiguracion.SISTRAGES_PWD);
-	}
+        final RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getInterceptors().add(
+                new BasicAuthorizationInterceptor(getUser(), getPassword()));
+        return restTemplate.getForObject(
+                getUrl() + "/dominioListaFija/" + dominio.getIdentificador(),
+                RValoresDominio.class);
 
-	/**
-	 * Obtiene el usuario
-	 *
-	 * @return
-	 */
-	private String getUser() {
-		return configuracionComponent.obtenerPropiedadConfiguracion(TypePropiedadConfiguracion.SISTRAGES_USR);
-	}
+    }
 
-	/**
-	 * Obtiene la url.
-	 *
-	 * @return
-	 */
-	private String getUrl() {
-		return configuracionComponent.obtenerPropiedadConfiguracion(TypePropiedadConfiguracion.SISTRAGES_URL);
-	}
+    /**** Private functions. **/
+    /**
+     * Obtiene el password.
+     *
+     * @return
+     */
+    private String getPassword() {
+        return configuracionComponent.obtenerPropiedadConfiguracion(
+                TypePropiedadConfiguracion.SISTRAGES_PWD);
+    }
+
+    /**
+     * Obtiene el usuario
+     *
+     * @return
+     */
+    private String getUser() {
+        return configuracionComponent.obtenerPropiedadConfiguracion(
+                TypePropiedadConfiguracion.SISTRAGES_USR);
+    }
+
+    /**
+     * Obtiene la url.
+     *
+     * @return
+     */
+    private String getUrl() {
+        return configuracionComponent.obtenerPropiedadConfiguracion(
+                TypePropiedadConfiguracion.SISTRAGES_URL);
+    }
 
 }
