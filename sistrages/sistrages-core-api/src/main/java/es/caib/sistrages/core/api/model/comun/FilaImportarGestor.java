c8a0051a6cf5dc11a428f80ed79f3337f5c858f8
jparedes-indra <jparedes@indra.es>
2021-05-10 22:41:26 +0200

    Se añaden ficheros para la versión 1.2.0

---
+++ sistrages/sistrages-core-api/src/main/java/es/caib/sistrages/core/api/model/comun/FilaImportarGestor.java
@@ -0,0 +1,225 @@
+package es.caib.sistrages.core.api.model.comun;
+
+import es.caib.sistrages.core.api.model.ConfiguracionAutenticacion;
+import es.caib.sistrages.core.api.model.GestorExternoFormularios;
+import es.caib.sistrages.core.api.model.types.TypeImportarAccion;
+import es.caib.sistrages.core.api.model.types.TypeImportarEstado;
+import es.caib.sistrages.core.api.model.types.TypeImportarExiste;
+import es.caib.sistrages.core.api.model.types.TypeImportarResultado;
+
+/**
+ * Fila gestor formulario externos importar.
+ *
+ * @author Indra
+ *
+ */
+public class FilaImportarGestor extends FilaImportarBase {
+
+	/** FormateadorFormulario . **/
+	private GestorExternoFormularios gestor;
+
+	/** FormateadorFormulario actual. **/
+	private GestorExternoFormularios gestorActual;
+
+	/** Configuracion autentiticacion actual **/
+	private ConfiguracionAutenticacion configuracionAutenticacionActual;
+
+	/**
+	 * Indica si está correcto el formateador. Se utiliza principalmente porque en
+	 * Importar Trámite, son todos de tipo info y hay que saber cual se tiene que
+	 * quitar.
+	 **/
+	private boolean correcto;
+
+	/** Constructor básico. **/
+	public FilaImportarGestor() {
+		super();
+	}
+
+	/**
+	 * Constructor.
+	 */
+	public FilaImportarGestor(final GestorExternoFormularios iGestor,
+			final GestorExternoFormularios iGestorActual, final TypeImportarAccion iAccion,
+			final TypeImportarExiste iExiste, final TypeImportarEstado iEstado, final TypeImportarResultado iTypeAccion,
+			final String iMensaje, final boolean correcto, final ConfiguracionAutenticacion config) {
+		super();
+		this.setGestor(iGestor);
+		this.setGestorActual(iGestorActual);
+		this.accion = iAccion;
+		this.estado = iEstado;
+		this.existe = iExiste;
+		this.resultado = iTypeAccion;
+		this.mensaje = iMensaje;
+		this.configuracionAutenticacionActual = config;
+		this.setCorrecto(correcto);
+	}
+
+
+	/**
+	 * @return the correcto
+	 */
+	public boolean isCorrecto() {
+		return correcto;
+	}
+
+	/**
+	 * @return the gestor
+	 */
+	public GestorExternoFormularios getGestor() {
+		return gestor;
+	}
+
+	/**
+	 * @return the gestorActual
+	 */
+	public GestorExternoFormularios getGestorActual() {
+		return gestorActual;
+	}
+
+	/**
+	 * @param gestorActual the gestorActual to set
+	 */
+	public void setGestorActual(GestorExternoFormularios gestorActual) {
+		this.gestorActual = gestorActual;
+	}
+
+	/**
+	 * @param gestor the gestor to set
+	 */
+	public void setGestor(GestorExternoFormularios gestor) {
+		this.gestor = gestor;
+	}
+
+	/**
+	 * @return the configuracionAutenticacionActual
+	 */
+	public ConfiguracionAutenticacion getConfiguracionAutenticacionActual() {
+		return configuracionAutenticacionActual;
+	}
+
+	/**
+	 * @param configuracionAutenticacionActual the configuracionAutenticacionActual to set
+	 */
+	public void setConfiguracionAutenticacionActual(ConfiguracionAutenticacion configuracionAutenticacionActual) {
+		this.configuracionAutenticacionActual = configuracionAutenticacionActual;
+	}
+
+	/**
+	 * @param correcto the correcto to set
+	 */
+	public void setCorrecto(final boolean correcto) {
+		this.correcto = correcto;
+	}
+
+	/**
+	 * Crea un elemento FilaImportarFormateador de tipo IT (Importar Tramite) cuando
+	 * NO existe el formateador o si existe pero está desactivado la
+	 * personalizacion.
+	 *
+	 * @param gestor
+	 * @param gestorActual
+	 * @param mensaje
+	 * @return
+	 */
+	public static FilaImportarGestor crearITgestor(final GestorExternoFormularios gestor,
+			final GestorExternoFormularios gestorActual, final String mensaje, final ConfiguracionAutenticacion config) {
+
+		TypeImportarExiste existe;
+		if (gestorActual == null) {
+			existe = TypeImportarExiste.NO_EXISTE;
+		} else {
+			existe = TypeImportarExiste.EXISTE;
+		}
+
+		return new FilaImportarGestor(gestor, gestorActual, TypeImportarAccion.NADA, existe,
+				TypeImportarEstado.ERROR, TypeImportarResultado.INFO, mensaje, false, config);
+
+	}
+
+	/**
+	 * Crea un elemento FilaImportarFormateador de tipo IT (Importar Tramite) cuando
+	 * existe el formateador
+	 *
+	 * @param gestor
+	 * @param gestorActual
+	 * @param literal
+	 * @return
+	 */
+	public static FilaImportarGestor creaITgestorExisteMalConf(final GestorExternoFormularios gestor,
+			final GestorExternoFormularios gestorActual, String mensaje, final ConfiguracionAutenticacion config) {
+		return new FilaImportarGestor(gestor, gestorActual, TypeImportarAccion.REEMPLAZAR,
+				TypeImportarExiste.EXISTE, TypeImportarEstado.REVISADO, TypeImportarResultado.WARNING, mensaje, true, config);
+	}
+
+	/**
+	 * Crea un elemento FilaImportarFormateador de tipo IT (Importar Tramite) cuando
+	 * existe el formateador
+	 *
+	 * @param gestor
+	 * @param gestorActual
+	 * @param literal
+	 * @return
+	 */
+	public static FilaImportarGestor creaITgestorExiste(final GestorExternoFormularios gestor,
+			final GestorExternoFormularios gestorActual, final ConfiguracionAutenticacion config) {
+		return new FilaImportarGestor(gestor, gestorActual, TypeImportarAccion.REEMPLAZAR,
+				TypeImportarExiste.EXISTE, TypeImportarEstado.REVISADO, TypeImportarResultado.INFO, null, true, config);
+	}
+
+	/**
+	 * Crea un elemento FilaImportarFormateador de tipo IT (Importar Tramite) cuando
+	 * existe el formateador
+	 *
+	 * @param gestor
+	 * @param gestorActual
+	 * @param literal
+	 * @return
+	 */
+	public static FilaImportarGestor creaITgestorExisteSinConfig(final GestorExternoFormularios gestor,
+			final GestorExternoFormularios gestorActual, final String mensaje, final ConfiguracionAutenticacion config) {
+		return new FilaImportarGestor(gestor, gestorActual, TypeImportarAccion.REEMPLAZAR,
+				TypeImportarExiste.EXISTE, TypeImportarEstado.REVISADO, TypeImportarResultado.WARNING, mensaje, true, config);
+	}
+
+	/**
+	 * Crea un elemento FilaImportarFormateador de tipo CC (Cuaderno Cargar) cuando
+	 * NO existe el formateador o si existe pero está desactivado la
+	 * personalizacion.
+	 *
+	 * @param gestor
+	 * @param gestorActual
+	 * @param mensaje
+	 * @return
+	 */
+	public static FilaImportarGestor crearCCgestorError(final GestorExternoFormularios gestor,
+			final GestorExternoFormularios gestorActual, final String mensaje, final ConfiguracionAutenticacion config) {
+
+		TypeImportarExiste existe;
+		if (gestorActual == null) {
+			existe = TypeImportarExiste.NO_EXISTE;
+		} else {
+			existe = TypeImportarExiste.EXISTE;
+		}
+
+		return new FilaImportarGestor(gestor, gestorActual, TypeImportarAccion.NADA, existe,
+				TypeImportarEstado.ERROR, TypeImportarResultado.ERROR, mensaje, false, config);
+
+	}
+
+	/**
+	 * Crea un elemento FilaImportarFormateador de tipo CC (Cuaderno Cargar) cuando
+	 * existe el formateador
+	 *
+	 * @param gestor
+	 * @param gestorActual
+	 * @param mensaje
+	 * @return
+	 */
+	public static FilaImportarGestor crearCCformateadorOk(final GestorExternoFormularios gestor,
+			final GestorExternoFormularios gestorActual, final ConfiguracionAutenticacion config) {
+		return new FilaImportarGestor(gestor, gestorActual, TypeImportarAccion.MANTENER,
+				TypeImportarExiste.EXISTE, TypeImportarEstado.REVISADO, TypeImportarResultado.INFO, null, true, config);
+	}
+
+}
---
+++ sistramit/sistramit-rest-api/src/main/java/es/caib/sistramit/rest/api/externa/v1/RTramiteFinalizado.java
@@ -0,0 +1,218 @@
+package es.caib.sistramit.rest.api.externa.v1;
+
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+/**
+ * Tramite en persistencia
+ *
+ * @author Indra
+ *
+ */
+@ApiModel(value = "RTramiteFinalizado", description = "Tramite finalizado")
+public final class RTramiteFinalizado {
+
+	@ApiModelProperty(value = "Id. Sesion Tramitacion")
+	private String idSesionTramitacion;
+	@ApiModelProperty(value = "Idioma")
+	private String idioma;
+	@ApiModelProperty(value = "Id. Tramite")
+	private String idTramite;
+	@ApiModelProperty(value = "Version Tramite")
+	private int versionTramite;
+	@ApiModelProperty(value = "Descripcion Tramite")
+	private String descripcionTramite;
+	@ApiModelProperty(value = "Id procedimiento SIA")
+	private String idProcedimientoSIA;
+	@ApiModelProperty(value = "Fecha finalización")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+	private Date fechaFin;
+	@ApiModelProperty(value = "Tipo autenticación: Autenticado (c) / Anónimo (a).")
+	private String autenticacion;
+	@ApiModelProperty(value = "Metodo Autenticacion (ANONIMO / CLAVE_CERTIFICADO / CLAVE_PIN / CLAVE_PERMANENTE)")
+	private String metodoAutenticacion;
+	@ApiModelProperty(value = "Nif")
+	private String nif;
+	@ApiModelProperty(value = "Nombre y apellidos")
+	private String nombreApellidos;
+	@ApiModelProperty(value = "Número registro")
+	private String numeroRegistro;
+
+	public String getIdSesionTramitacion() {
+		return idSesionTramitacion;
+	}
+
+	public void setIdSesionTramitacion(final String idSesionTramitacion) {
+		this.idSesionTramitacion = idSesionTramitacion;
+	}
+
+	public String getIdTramite() {
+		return idTramite;
+	}
+
+	public void setIdTramite(final String idTramite) {
+		this.idTramite = idTramite;
+	}
+
+	public int getVersionTramite() {
+		return versionTramite;
+	}
+
+	public void setVersionTramite(final int versionTramite) {
+		this.versionTramite = versionTramite;
+	}
+
+	public String getDescripcionTramite() {
+		return descripcionTramite;
+	}
+
+	public void setDescripcionTramite(final String descripcionTramite) {
+		this.descripcionTramite = descripcionTramite;
+	}
+
+	public String getIdioma() {
+		return idioma;
+	}
+
+	public void setIdioma(final String idioma) {
+		this.idioma = idioma;
+	}
+
+	/**
+	 * Método de acceso a idProcedimientoSIA.
+	 *
+	 * @return idProcedimientoSIA
+	 */
+	public String getIdProcedimientoSIA() {
+		return idProcedimientoSIA;
+	}
+
+	/**
+	 * Método para establecer idProcedimientoSIA.
+	 *
+	 * @param idProcedimientoSIA
+	 *                               idProcedimientoSIA a establecer
+	 */
+	public void setIdProcedimientoSIA(final String idProcedimientoSIA) {
+		this.idProcedimientoSIA = idProcedimientoSIA;
+	}
+
+	/**
+	 * Método de acceso a fechaFin.
+	 *
+	 * @return fechaFin
+	 */
+	public Date getFechaFin() {
+		return fechaFin;
+	}
+
+	/**
+	 * Método para establecer fechaFin.
+	 *
+	 * @param fechaFin
+	 *                     fechaFin a establecer
+	 */
+	public void setFechaFin(final Date fechaFin) {
+		this.fechaFin = fechaFin;
+	}
+
+	/**
+	 * Método de acceso a autenticacion.
+	 *
+	 * @return autenticacion
+	 */
+	public String getAutenticacion() {
+		return autenticacion;
+	}
+
+	/**
+	 * Método para establecer autenticacion.
+	 *
+	 * @param autenticacion
+	 *                          autenticacion a establecer
+	 */
+	public void setAutenticacion(final String autenticacion) {
+		this.autenticacion = autenticacion;
+	}
+
+	/**
+	 * Método de acceso a metodoAutenticacion.
+	 *
+	 * @return metodoAutenticacion
+	 */
+	public String getMetodoAutenticacion() {
+		return metodoAutenticacion;
+	}
+
+	/**
+	 * Método para establecer metodoAutenticacion.
+	 *
+	 * @param metodoAutenticacion
+	 *                                metodoAutenticacion a establecer
+	 */
+	public void setMetodoAutenticacion(final String metodoAutenticacion) {
+		this.metodoAutenticacion = metodoAutenticacion;
+	}
+
+	/**
+	 * Método de acceso a nif.
+	 *
+	 * @return nif
+	 */
+	public String getNif() {
+		return nif;
+	}
+
+	/**
+	 * Método para establecer nif.
+	 *
+	 * @param nif
+	 *                nif a establecer
+	 */
+	public void setNif(final String nif) {
+		this.nif = nif;
+	}
+
+	/**
+	 * Método de acceso a nombreApellidos.
+	 *
+	 * @return nombreApellidos
+	 */
+	public String getNombreApellidos() {
+		return nombreApellidos;
+	}
+
+	/**
+	 * Método para establecer nombreApellidos.
+	 *
+	 * @param nombreApellidos
+	 *                            nombreApellidos a establecer
+	 */
+	public void setNombreApellidos(final String nombreApellidos) {
+		this.nombreApellidos = nombreApellidos;
+	}
+
+	/**
+	 * Método de acceso a numeroRegistro.
+	 *
+	 * @return numeroRegistro
+	 */
+	public String getNumeroRegistro() {
+		return numeroRegistro;
+	}
+
+	/**
+	 * Método para establecer numeroRegistro.
+	 *
+	 * @param numeroRegistro
+	 *                           numeroRegistro a establecer
+	 */
+	public void setNumeroRegistro(final String numeroRegistro) {
+		this.numeroRegistro = numeroRegistro;
+	}
+
+}
