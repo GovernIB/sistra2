9e6d4e1fbd68f0bb6e6579f2c9308ec48afc236e
jparedes-indra <jparedes@indra.es>
2022-03-14 09:47:35 +0100

    Se actualiza el script que ha de ejecutarse en BBDD para pasar de la versión 1.2.1 a la 1.2.2 de SISTRA2.

--- sistramit/scripts/bbdd/1.2/sistramit_update_from_1.2.1_to_1.2.2.sql
+++ sistramit/scripts/bbdd/1.2/sistramit_update_from_1.2.1_to_1.2.2.sql
@@ -1,3 +1,18 @@
 /* Logos x UA */
 alter table STT_FORMUL add   SFR_DIR3PRO          VARCHAR2(20 CHAR);
 comment on column STT_FORMUL.SFR_DIR3PRO is 'Dir3 responsable procedimiento';
+
+/* #496 check de debug falla en intentar desar el seient a BBDD  */
+ALTER TABLE STT_LOGINT
+ADD LOG_EVEDET_2 CLOB;
+
+UPDATE STT_LOGINT 
+SET LOG_EVEDET_2 = TO_CLOB (LOG_EVEDET);
+
+ALTER TABLE STT_LOGINT
+DROP COLUMN LOG_EVEDET;
+
+ALTER TABLE STT_LOGINT
+RENAME COLUMN LOG_EVEDET_2 TO LOG_EVEDET;
+
+COMMENT ON COLUMN STT_LOGINT.LOG_EVEDET IS 'Detalle evento (depende del evento). Permite establecer info adicional mediante una lista de campos de información particulares del evento con formato: propiedad1=valor1#@#propiedad2=valor2';
